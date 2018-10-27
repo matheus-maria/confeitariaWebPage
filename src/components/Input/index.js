e2d4c7d83b1d2a4465f1b46645053a66d38d06f1
Matheus Maria <mmatheus.maria@gmail.com>
2018-10-27 16:22:23 -0300

    Input Component Updated

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -8,19 +8,20 @@ export default class Input extends Component {
     }
 
     state = {
-        value:''
+        value:'',        
     };    
 
     valueChange = (value) => {
         this.setState({value});  
         this.props.onChange(value);
-    }
+    }   
 
     render() {
         return(
             <div className="col-3 input-effect">
-                <input className="effect-16" type="text" placeholder="" onChange={value => this.valueChange(value.target.value)}/>
-                <label hidden={this.state.value.length == 0 ? false:true} >{this.props.title}</label>
+                <input className={"effect-16"} type="text" placeholder="" onChange={value => this.valueChange(value.target.value)} />
+                <label hidden={this.state.value == 0 ? false:true}>{this.props.title}</label>
+                <label id="labelFixed" hidden={this.state.value == 0 ? true:false}>{this.props.title}</label>
                 <span className="focus-border"></span>
             </div>
         )  
